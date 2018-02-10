26f4697b99ad9ff3cbd6be1f179573ebcfc0f281
Daichi Fukuoka <daichi.dc.fukuoka@gmai.com>
2018-02-10 14:29:59 +0900

    overload operator = and operator * now like the following can be achieved:
    type(matrix) :: a, b, c, d
    
    call a%init(size)
    call b%init(size)
    call c%init(size)
    call a%set_mat(-1.0d0, 1.0d0, 5555)
    call b%set_mat(-2.0d0, 2.0d0, 7777)
    
    c = a*b
    d = c

--- test.F90
+++ test.F90
@@ -1,146 +1,143 @@
 module mod1
   implicit none
-  type mm
+  private ::gen_rand
+
+  interface operator(*)
+     procedure mm_
+  end interface operator(*)
+
+  type matrix
      integer :: size
-     real(8),allocatable,dimension(:, :) :: a, b, c
-     contains
-       generic   :: init  => init0, init1
-       procedure :: init0 => init_, init1 => init__
-       procedure :: set_mat => set_mat_
-       procedure :: mm    => mm_
-       procedure :: show_mat => show_mat_
-       final   :: fini
-    end type mm
-  contains
-    subroutine gen_rand(size, a, val_min, val_max, seed)
-      use mkl_vsl_type
-      use mkl_vsl
-      implicit none
-      integer,intent(in) :: size
-      real(8),dimension(size, size),intent(out) :: a
-      real(8),intent(in) :: val_min, val_max
-      integer,intent(in) :: seed
-      integer::ierr
-      integer::brng, method
-      type(vsl_stream_state)::stream
-
-      brng   = vsl_brng_mt19937
-!     for older intel MKL
-!     method = vsl_method_duniform_std
-      method = vsl_rng_method_uniform_std_accurate
-
-      ierr = vslnewstream(stream, brng, seed)
-      ierr = vdrnguniform(method, stream, size, a, val_min, val_max)
-      ierr = vsldeletestream(stream)
-    end subroutine gen_rand
-    
-  subroutine init_(this)
+     real(8),allocatable,dimension(:, :) :: mat
+   contains
+     generic   :: init          => init0, init1
+     procedure :: init0         => init0_, init1 => init1_
+     procedure :: set_mat       => set_mat_
+     procedure :: show_mat      => show_mat_
+     procedure :: equal         => equal_
+     generic   :: assignment(=) => equal
+     final     :: fini
+  end type matrix
+  
+contains
+  subroutine gen_rand(size, a, val_min, val_max, seed)
+    use mkl_vsl_type
+    use mkl_vsl
     implicit none
-    class(mm),intent(inout) :: this
+    integer,intent(in) :: size
+    real(8),dimension(size, size),intent(out) :: a
+    real(8),intent(in) :: val_min, val_max
+    integer,intent(in) :: seed
+    integer::ierr
+    integer::brng, method
+    type(vsl_stream_state)::stream
+
+    brng   = vsl_brng_mt19937
+    !     for older intel MKL
+    !     method = vsl_method_duniform_std
+    method = vsl_rng_method_uniform_std_accurate
+
+    ierr = vslnewstream(stream, brng, seed)
+    ierr = vdrnguniform(method, stream, size, a, val_min, val_max)
+    ierr = vsldeletestream(stream)
+  end subroutine gen_rand
+
+  subroutine init0_(this)
+    implicit none
+    class(matrix),intent(inout) :: this
     integer :: size
 
     size = 8
     this%size = size
-    allocate(this%a(size, size), this%b(size, size), this%c(size, size))
-    this%a(:, :) = 0.0d0
-    this%b(:, :) = 0.0d0
-    this%c(:, :) = 0.0d0
-  end subroutine init_
-  
-  subroutine init__(this, size)
+    allocate(this%mat(size, size))
+    !$omp parallel
+    !$omp workshare
+    this%mat(:, :) = 0.0d0
+    !$omp end workshare
+    !$omp end parallel
+  end subroutine init0_
+
+  subroutine init1_(this, size)
     implicit none
-    class(mm),intent(inout) :: this
+    class(matrix),intent(inout) :: this
     integer,intent(in) :: size
 
     this%size = size
-    allocate(this%a(size, size), this%b(size, size), this%c(size, size))
-    this%a(:, :) = 0.0d0
-    this%b(:, :) = 0.0d0
-    this%c(:, :) = 0.0d0
-  end subroutine init__
+    allocate(this%mat(size, size))
+    this%mat(:, :) = 0.0d0
+  end subroutine init1_
 
-  subroutine set_mat_(this, mat, val_min, val_max, seed)
+  subroutine set_mat_(this, val_min, val_max, seed)
     implicit none
-    class(mm),intent(inout) :: this
-    character(len=1),intent(in) :: mat
+    class(matrix),intent(inout) :: this
     real(8),intent(in) :: val_min, val_max
     integer,intent(in) :: seed
 
-    if (mat == 'a') then
-       call gen_rand(this%size**2, this%a, val_min, val_max, seed)
-    else if (mat == 'b') then
-       call gen_rand(this%size**2, this%b, val_min, val_max, seed)
-    else if (mat == 'c') then
-       call gen_rand(this%size**2, this%c, val_min, val_max, seed)
-    end if
+    call gen_rand(this%size**2, this%mat, val_min, val_max, seed)
   end subroutine set_mat_
-  subroutine show_mat_(this, mat)
-    class(mm),intent(in) :: this
-    character(len=1) :: mat
+  
+  subroutine show_mat_(this)
+    class(matrix),intent(in) :: this
     integer :: i, j
 
-    if (mat == 'a') then
-       write(6, *) "A:"
-       do i = 1, this%size
-          do j = 1, this%size
-             write(6, '(1pe14.5)', advance="no") this%a(i, j)
-          end do
-          write(6, *)
+    do i = 1, this%size
+       do j = 1, this%size
+          write(6, '(1pe14.5)', advance="no") this%mat(i, j)
        end do
-    else if (mat == 'b') then
-       write(6, *) "B:"
-       do i = 1, this%size
-          do j = 1, this%size
-             write(6, '(1pe14.5)', advance="no") this%b(i, j)
-          end do
-          write(6, *)
-       end do
-    else if (mat == 'c') then
-       write(6, *) "C:"
-       do i = 1, this%size
-          do j = 1, this%size
-             write(6, '(1pe14.5)', advance="no") this%c(i, j)
-          end do
-          write(6, *)
-       end do
-    end if
-    
+       write(6, *)
+    end do
   end subroutine show_mat_
 
-  subroutine mm_(this)
+  function mm_(a, b) result(c)
     implicit none
-    class(mm),intent(inout) :: this
+    type(matrix),intent(in)  :: a, b
+    type(matrix) :: c
     integer :: i, j, k
+    integer :: size
 
+    size = a%size
+    call c%init(a%size)
     !$omp parallel do
-    do j = 1, this%size
-       do k = 1, this%size
-          do i = 1, this%size
-             this%c(i, j) = this%c(i, j) + this%a(i, k)*this%b(k ,j)
+    do j = 1, size
+       do k = 1, size
+          do i = 1, size
+             c%mat(i, j) = c%mat(i, j) + a%mat(i, k)*b%mat(k ,j)
           end do
        end do
     end do
-  end subroutine mm_
+  end function mm_
+
+  subroutine equal_(lhs, rhs)
+    implicit none
+    class(matrix),intent(out) :: lhs
+    class(matrix),intent(in)  :: rhs
+
+    call lhs%init1(rhs%size)
+    
+    !$omp parallel
+    !$omp workshare
+    lhs%mat(:, :) = rhs%mat(:, :)
+    !$omp end workshare
+    !$omp end parallel
+  end subroutine equal_
 
   subroutine fini(this)
     implicit none
-    type(mm),intent(inout) :: this
+    type(matrix),intent(inout) :: this
 
     this%size = 0
-    if (allocated(this%a)) deallocate(this%a)
-    if (allocated(this%b)) deallocate(this%b)
-    if (allocated(this%c)) deallocate(this%c)
+    if (allocated(this%mat)) deallocate(this%mat)
   end subroutine fini
 end module mod1
 
 program main
   use mod1
   implicit none
-  type(mm) :: mm1
+  type(matrix) :: a, b, c, d
   integer :: size
   character(len=16) :: argv1
   integer :: i
-  
+
   if (iargc() == 0) then
      size = 4
   else
@@ -148,13 +145,25 @@ program main
      read(argv1, *) size
   end if
   write(6, *) "size:", size
+
+  call a%init(size)
+  call b%init(size)
+  call c%init(size)
+  call a%set_mat(-1.0d0, 1.0d0, 5555)
+  call b%set_mat(-2.0d0, 2.0d0, 7777)
+
+  c = a*b
+  d = c
+
+  write(6, *) "A:"
+  call a%show_mat
+  write(6, *) "B:"
+  call b%show_mat
+  write(6, *) "C:"
+  call c%show_mat
+  write(6, *) "D:"
+  call d%show_mat
   
-  call mm1%init(size)
-  call mm1%set_mat('a', -1.0d0, 1.0d0, 5555)
-  call mm1%set_mat('b', -2.0d0, 2.0d0, 7777)
-  call mm1%mm
-  call mm1%show_mat('a')
-  call mm1%show_mat('b')
-  call mm1%show_mat('c')
   stop
 end program main
+
